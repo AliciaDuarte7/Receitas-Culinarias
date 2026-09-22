e19fd70896cd43b7c7515944d60faafd672d695e
Alicia Duarte <aliciacdoliveira@gmail.com>
2026-09-22 17:08:34 -0300

    Model

--- backend/src/main/java/com/receitas/backend/model/Receita.java
+++ backend/src/main/java/com/receitas/backend/model/Receita.java
@@ -14,4 +14,48 @@ public class Receita {
 
     @Column(nullable = false, length = 100)
     private String nome;
+
+    @Column(nullable = false,)
+    private String ingredientes;
+
+    @Column()
+    private Integer tempoPreparoMin;
+
+    @Column()
+    private String dificuldade;
+
+    @Column()
+    private Boolean favorita;
+
+    public Receita(String nome, String ingredientes, Integer tempoPreparoMin, String dificuldade, Boolean favorita){
+        this.nome = "nome";
+        this.ingredientes = "ingredientes";
+        this.tempoPreparoMin = "tempoPreparoMin";
+        this.dificuldade = "dificuldade";
+        this.favorita = "favorita";
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getIngredientes() {
+        return ingredientes;
+    }
+
+    public Integer getTempoPreparoMin() {
+        return tempoPreparoMin;
+    }
+
+    public String getDificuldade() {
+        return dificuldade;
+    }
+
+    public Boolean getFavorita() {
+        return favorita;
+    }
 }
